632bd52d533e4ceb465625eeede7648b09bedad7
Rishav Rout <rout.rishav@gmail.com>
2024-04-17 15:33:08 -0700

    linting

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,10 @@ import './App.css';
 import {getRosters} from './sleeper-api/sleeper-api';
 
 function App() {
-  useEffect(() => {
-    getRosters('1048770475687571456');
-  });
-  return (
-    <div className="App">
-      ayo
-    </div>
-  );
+    useEffect(() => {
+        getRosters('1048770475687571456');
+    });
+    return <div className="App">ayo</div>;
 }
 
 export default App;
--- src/sleeper-api/sleeper-api.ts
+++ src/sleeper-api/sleeper-api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 export type league = {
     league_id: string;
@@ -18,15 +18,19 @@ export type roster = {
 
         waiver_position: number;
         waiver_budget_used: number;
-    }
-}
+    };
+};
 
 export async function getLeague(leagueId: string): Promise<league> {
-    const response = await axios.get(`https://api.sleeper.app/v1/league/${leagueId}`);
+    const response = await axios.get(
+        `https://api.sleeper.app/v1/league/${leagueId}`
+    );
     return response.data as league;
 }
 
 export async function getRosters(leagueId: string): Promise<roster[]> {
-    const response = await axios.get(`https://api.sleeper.app/v1/league/${leagueId}/rosters`);
-    return response.data;;
+    const response = await axios.get(
+        `https://api.sleeper.app/v1/league/${leagueId}/rosters`
+    );
+    return response.data;
 }
