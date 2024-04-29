05722cf2610ed7d0d32d54f2327ff007be5203ad
Rishav Rout <rout.rishav@gmail.com>
2024-04-28 17:02:55 -0700

    basic input response

--- src/pages/Player/Search/PlayerSearch.tsx
+++ src/pages/Player/Search/PlayerSearch.tsx
@@ -1,3 +1,22 @@
+import {TextField} from '@mui/material';
+import styles from './PlayerSearch.module.css';
+import {useState} from 'react';
 export default function PlayerSearch() {
-    return <>yeet</>;
+    const [searchInput, setSeachInput] = useState('');
+
+    function searchResults() {
+        return <>{searchInput}</>;
+    }
+
+    return (
+        <div className={styles.playerSearch}>
+            <TextField
+                onChange={e => {
+                    setSeachInput(e.target.value);
+                }}
+                className={styles.input}
+            ></TextField>
+            {searchResults()}
+        </div>
+    );
 }
