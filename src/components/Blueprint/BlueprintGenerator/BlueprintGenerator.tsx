3f1dbdbe14df7dec7995e8b7efae331f039a66f6
Rishav Rout <rout.rishav@gmail.com>
2024-05-30 21:51:22 -0700

    module refactor

--- src/components/Blueprint/BlueprintGenerator/BlueprintGenerator.tsx
+++ src/components/Blueprint/BlueprintGenerator/BlueprintGenerator.tsx
@@ -1,30 +1,14 @@
-import {useEffect, useRef, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {
-    useAdpData,
     useFetchRosters,
     useLeagueIdFromUrl,
     usePlayerData,
 } from '../../../hooks/hooks';
-import {
-    Player,
-    Roster,
-    User,
-    getAllUsers,
-} from '../../../sleeper-api/sleeper-api';
+import {Roster, User, getAllUsers} from '../../../sleeper-api/sleeper-api';
 import styles from './BlueprintGenerator.module.css';
 import {teamSelectComponent} from '../../Team/TeamPage/TeamPage';
 import {NONE_TEAM_ID} from '../../../consts/urlParams';
-import {
-    Button,
-    FormControl,
-    InputLabel,
-    MenuItem,
-    Select,
-    SelectChangeEvent,
-} from '@mui/material';
-import {sortBySearchRank} from '../../Player/Search/PlayerSearch';
-import {toPng} from 'html-to-image';
-import {FANTASY_POSITIONS} from '../../../consts/fantasy';
+import CornerstoneModule from './modules/cornerstone/CornerstoneModule';
 export default function BlueprintGenerator() {
     const [leagueId] = useLeagueIdFromUrl();
     const [teamId, setTeamId] = useState(NONE_TEAM_ID);
@@ -32,23 +16,16 @@ export default function BlueprintGenerator() {
     const [allUsers, setAllUsers] = useState<User[]>([]);
     const [roster, setRoster] = useState<Roster>();
     const playerData = usePlayerData();
-    const [_, getAdp] = useAdpData();
-    const specifiedUser = allUsers?.[+teamId];
-    const componentRef = useRef(null);
-    const [cornerstones, setCornerstones] = useState(
-        new Map<string, string[]>(FANTASY_POSITIONS.map(pos => [pos, []]))
-    );
+    const [specifiedUser, setSpecifiedUser] = useState<User>();
+    useEffect(() => {
+        if (!allUsers.length || !hasTeamId()) return;
+        setSpecifiedUser(allUsers?.[+teamId]);
+    }, [allUsers, teamId]);
 
     function getRosterFromTeamIdx(idx: number) {
         if (allUsers.length === 0 || !rosters) return;
         const ownerId = allUsers[idx].user_id;
         return rosters.find(r => r.owner_id === ownerId);
-    }
-
-    function isCornerstone(player?: Player) {
-        if (!player) return false;
-        const adp = getAdp(`${player.first_name} ${player.last_name}`);
-        return adp <= 75 && adp >= 0;
     }
 
     useEffect(() => {
@@ -59,21 +36,6 @@ export default function BlueprintGenerator() {
         const newRoster = getRosterFromTeamIdx(+teamId);
         if (!newRoster) throw new Error('roster not found');
 
-        const cornerstones = newRoster.players
-            .map(playerId => playerData[playerId])
-            .filter(isCornerstone);
-        setCornerstones(
-            new Map<string, string[]>(
-                FANTASY_POSITIONS.map(pos => [
-                    pos,
-                    cornerstones
-                        .filter(player =>
-                            player.fantasy_positions.includes(pos)
-                        )
-                        .map(player => player.player_id),
-                ])
-            )
-        );
         setRoster(newRoster);
     }, [rosters, teamId, playerData]);
 
@@ -82,137 +44,19 @@ export default function BlueprintGenerator() {
         getAllUsers(leagueId).then(users => setAllUsers(users));
     }, [leagueId]);
 
-    function playerSelectComponent(
-        players: string[],
-        selectedPlayerIds: string[],
-        onChange: (event: SelectChangeEvent<string[]>) => void,
-        position: string
-    ) {
-        if (!playerData) return <></>;
-        return (
-            <FormControl
-                style={{
-                    margin: '4px',
-                }}
-            >
-                <InputLabel>{position}</InputLabel>
-                <Select
-                    value={selectedPlayerIds}
-                    label={position}
-                    onChange={onChange}
-                    multiple={true}
-                >
-                    {players
-                        .map(playerId => playerData[playerId])
-                        .filter(
-                            player =>
-                                player &&
-                                player.fantasy_positions.includes(position)
-                        )
-                        .sort(sortBySearchRank)
-                        .map((player, idx) => (
-                            <MenuItem value={player.player_id} key={idx}>
-                                {player.first_name} {player.last_name}
-                            </MenuItem>
-                        ))}
-                </Select>
-            </FormControl>
-        );
-    }
-
-    function cornerstoneAssetsComponent() {
-        if (!playerData) return <></>;
-        return (
-            <div
-                className={styles.cornerstoneAssetsComponent}
-                ref={componentRef}
-            >
-                <div className={styles.positions}>
-                    {FANTASY_POSITIONS.map(pos => (
-                        <div className={styles.column}>
-                            <div
-                                className={`${styles.positionChip} ${styles[pos]}`}
-                            >
-                                {pos}
-                            </div>
-                            <div className={styles.cornerstoneList}>
-                                {cornerstones
-                                    .get(pos)!
-                                    .map(playerId => playerData[playerId])
-                                    .map(player => {
-                                        return (
-                                            <div>
-                                                {`${player.first_name} ${player.last_name}`.toLocaleUpperCase()}
-                                            </div>
-                                        );
-                                    })}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        );
-    }
-
-    function allPositionalSelectors() {
-        return FANTASY_POSITIONS.map(pos =>
-            playerSelectComponent(
-                roster?.players ?? [],
-                cornerstones.get(pos) ?? [],
-                (e: SelectChangeEvent<string[]>) => {
-                    const {
-                        target: {value},
-                    } = e;
-                    cornerstones.set(
-                        pos,
-                        typeof value === 'string' ? value.split(',') : value
-                    );
-                    setCornerstones(new Map(cornerstones));
-                },
-                pos
-            )
-        );
-    }
-
     function hasTeamId() {
         return teamId !== '' && teamId !== NONE_TEAM_ID;
-    }
-
-    function exportButton() {
-        return (
-            <Button
-                variant="outlined"
-                onClick={() =>
-                    toPng(
-                        document.getElementsByClassName(
-                            styles.cornerstoneAssetsComponent
-                        )[0] as HTMLElement,
-                        {backgroundColor: 'rgba(0, 0, 0, 0)'}
-                    ).then(dataUrl => {
-                        const link = document.createElement('a');
-                        link.href = dataUrl;
-                        link.download = `${specifiedUser?.username}_cornerstones.png`;
-                        document.body.appendChild(link);
-                        link.click();
-                        document.body.removeChild(link);
-                    })
-                }
-                style={{
-                    margin: '8px',
-                }}
-                className={styles.button}
-            >
-                Export As PNG
-            </Button>
-        );
     }
 
     return (
         <div className={styles.blueprintPage}>
             {teamSelectComponent(teamId, setTeamId, allUsers, specifiedUser)}
-            {hasTeamId() && cornerstoneAssetsComponent()}
-            {hasTeamId() && exportButton()}
-            {hasTeamId() && allPositionalSelectors()}
+            {hasTeamId() && (
+                <CornerstoneModule
+                    roster={roster}
+                    specifiedUser={specifiedUser}
+                />
+            )}
         </div>
     );
 }
---
+++ src/components/Blueprint/BlueprintGenerator/modules/cornerstone/CornerstoneModule.tsx
@@ -0,0 +1,182 @@
+import {useEffect, useRef, useState} from 'react';
+import {FANTASY_POSITIONS} from '../../../../../consts/fantasy';
+import {useAdpData, usePlayerData} from '../../../../../hooks/hooks';
+import styles from './../../BlueprintGenerator.module.css';
+import {
+    InputLabel,
+    SelectChangeEvent,
+    FormControl,
+    MenuItem,
+    Select,
+    Button,
+} from '@mui/material';
+import {sortBySearchRank} from '../../../../Player/Search/PlayerSearch';
+import {Player, Roster, User} from '../../../../../sleeper-api/sleeper-api';
+import {toPng} from 'html-to-image';
+export default function CornerstoneModule(props: {
+    roster?: Roster;
+    specifiedUser?: User;
+}) {
+    const {roster, specifiedUser} = props;
+    const playerData = usePlayerData();
+    const componentRef = useRef(null);
+    const [_, getAdp] = useAdpData();
+    const [cornerstones, setCornerstones] = useState(
+        new Map<string, string[]>(FANTASY_POSITIONS.map(pos => [pos, []]))
+    );
+
+    function isCornerstone(player?: Player) {
+        if (!player) return false;
+        // this is probably pretty brittle
+        const adp = getAdp(`${player.first_name} ${player.last_name}`);
+        return adp <= 75 && adp >= 0;
+    }
+
+    useEffect(() => {
+        if (!roster || !playerData) return;
+
+        const cornerstones = roster.players
+            .map(playerId => playerData[playerId])
+            .filter(isCornerstone);
+
+        setCornerstones(
+            new Map<string, string[]>(
+                FANTASY_POSITIONS.map(pos => [
+                    pos,
+                    cornerstones
+                        .filter(player =>
+                            player.fantasy_positions.includes(pos)
+                        )
+                        .map(player => player.player_id),
+                ])
+            )
+        );
+    }, [roster]);
+    function cornerstoneAssetsComponent() {
+        if (!playerData) return <></>;
+        return (
+            <div
+                className={styles.cornerstoneAssetsComponent}
+                ref={componentRef}
+            >
+                <div className={styles.positions}>
+                    {FANTASY_POSITIONS.map(pos => (
+                        <div className={styles.column}>
+                            <div
+                                className={`${styles.positionChip} ${styles[pos]}`}
+                            >
+                                {pos}
+                            </div>
+                            <div className={styles.cornerstoneList}>
+                                {cornerstones
+                                    .get(pos)!
+                                    .map(playerId => playerData[playerId])
+                                    .map(player => {
+                                        return (
+                                            <div>
+                                                {`${player.first_name} ${player.last_name}`.toLocaleUpperCase()}
+                                            </div>
+                                        );
+                                    })}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+    function playerSelectComponent(
+        players: string[],
+        selectedPlayerIds: string[],
+        onChange: (event: SelectChangeEvent<string[]>) => void,
+        position: string
+    ) {
+        if (!playerData) return <></>;
+        return (
+            <FormControl
+                style={{
+                    margin: '4px',
+                }}
+            >
+                <InputLabel>{position}</InputLabel>
+                <Select
+                    value={selectedPlayerIds}
+                    label={position}
+                    onChange={onChange}
+                    multiple={true}
+                >
+                    {players
+                        .map(playerId => playerData[playerId])
+                        .filter(
+                            player =>
+                                player &&
+                                player.fantasy_positions.includes(position)
+                        )
+                        .sort(sortBySearchRank)
+                        .map((player, idx) => (
+                            <MenuItem value={player.player_id} key={idx}>
+                                {player.first_name} {player.last_name}
+                            </MenuItem>
+                        ))}
+                </Select>
+            </FormControl>
+        );
+    }
+    function exportButton() {
+        return (
+            <Button
+                variant="outlined"
+                onClick={() =>
+                    toPng(
+                        document.getElementsByClassName(
+                            styles.cornerstoneAssetsComponent
+                        )[0] as HTMLElement,
+                        {backgroundColor: 'rgba(0, 0, 0, 0)'}
+                    ).then(dataUrl => {
+                        const link = document.createElement('a');
+                        link.href = dataUrl;
+                        link.download = `${specifiedUser?.metadata?.team_name}_cornerstones.png`;
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
+                    })
+                }
+                style={{
+                    margin: '8px',
+                }}
+                className={styles.button}
+            >
+                Export As PNG
+            </Button>
+        );
+    }
+
+    function allPositionalSelectors() {
+        return FANTASY_POSITIONS.map(pos =>
+            playerSelectComponent(
+                roster?.players ?? [],
+                cornerstones.get(pos) ?? [],
+                (e: SelectChangeEvent<string[]>) => {
+                    const {
+                        target: {value},
+                    } = e;
+                    cornerstones.set(
+                        pos,
+                        typeof value === 'string' ? value.split(',') : value
+                    );
+                    setCornerstones(new Map(cornerstones));
+                },
+                pos
+            )
+        );
+    }
+
+    return (
+        <>
+            {cornerstoneAssetsComponent()}
+            {exportButton()}
+            {allPositionalSelectors()}
+        </>
+    );
+}
