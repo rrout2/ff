859ab37d7da52ec2e916281d602455240bcd197d
Rishav Rout <rout.rishav@gmail.com>
2024-06-11 16:49:04 -0500

    fix transaction log

--- src/components/Transactions/AllTransactions.tsx
+++ src/components/Transactions/AllTransactions.tsx
@@ -1,4 +1,3 @@
-import {useQuery} from '@tanstack/react-query';
 import {
     useFetchUsers,
     useFetchRosters,
@@ -11,7 +10,7 @@ import {
     User,
     getTransacations,
 } from '../../sleeper-api/sleeper-api';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import PlayerPreview from '../Player/PlayerPreview/PlayerPreview';
 import styles from './AllTransactions.module.css';
 import Menu from '../Menu/Menu';
@@ -26,19 +25,12 @@ export default function AllTransactions() {
     const playerData = usePlayerData();
     const navigate = useNavigate();
 
-    const {data: fetchedTransactions} = useQuery({
-        queryKey: [leagueId],
-        enabled: leagueId !== '',
-        queryFn: async () => {
-            const res = await getTransacations(leagueId);
-            setTransactions(res);
-            return res;
-        },
-        staleTime: 10000,
-    });
-    const [transactions, setTransactions] = useState<Transaction[]>(
-        fetchedTransactions ?? []
-    );
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+    useEffect(() => {
+        if (!leagueId) return;
+        getTransacations(leagueId).then(txns => setTransactions(txns));
+    }, [leagueId]);
 
     function findUser(userId: number) {
         if (!allUsers) return;
@@ -67,50 +59,96 @@ export default function AllTransactions() {
         return addsByUser;
     }
 
+    function getDropsByUser({drops, draft_picks}: Transaction) {
+        const dropsByUser = new Map<User, (string | TradedPick)[]>();
+
+        for (const playerId in drops) {
+            const user = findUser(drops[playerId])!;
+            if (!dropsByUser.has(user)) {
+                dropsByUser.set(user, []);
+            }
+            dropsByUser.set(user, [playerId, ...dropsByUser.get(user)!]);
+        }
+        draft_picks.forEach(pick => {
+            const user = findUser(pick.previous_owner_id)!;
+            if (!dropsByUser.has(user)) {
+                dropsByUser.set(user, []);
+            }
+            dropsByUser.set(user, [pick, ...dropsByUser.get(user)!]);
+        });
+        return dropsByUser;
+    }
+
+    function addDropPiece(addDrop: string | TradedPick) {
+        if (!playerData) return <></>;
+        if (typeof addDrop === 'string') {
+            return (
+                <PlayerPreview
+                    player={playerData[addDrop]}
+                    leagueId={leagueId}
+                    hideHeadshot={true}
+                />
+            );
+        }
+        return <div>{`${addDrop.season} Round ${addDrop.round}`}</div>;
+    }
+
     function singleTransactionComponent(t: Transaction) {
-        const {transaction_id} = t;
         if (!playerData || !allUsers) return <></>;
 
         const addsByUser = getAddsByUser(t);
+        const dropsByUser = getDropsByUser(t);
 
         if (addsByUser.size === 0) return <></>;
+        const isWaiverMove = t.roster_ids.length === 1;
+
+        const addedText = isWaiverMove ? 'added' : 'received';
+        const droppedText = isWaiverMove ? 'dropped' : 'sent';
 
         return (
-            <div key={transaction_id} className={styles.singleton}>
-                {Array.from(addsByUser).map(([user, adds], idx) => (
-                    <>
-                        <div
-                            className={
-                                styles.teamLabel +
-                                (idx > 0 ? ` ${styles.notFirst}` : '')
-                            }
-                            onClick={() => {
-                                const teamId = allUsers.findIndex(
-                                    u => u.user_id === user.user_id
-                                );
-                                navigate(
-                                    `../team?${LEAGUE_ID}=${leagueId}&${TEAM_ID}=${teamId}`
-                                );
-                            }}
-                        >
-                            {user.display_name} received:
-                        </div>
-                        {adds.map(add => {
-                            if (typeof add === 'string') {
-                                return (
-                                    <PlayerPreview
-                                        player={playerData[add]}
-                                        leagueId={leagueId}
-                                        hideHeadshot={true}
-                                    />
-                                );
-                            }
-                            return (
-                                <div>{`${add.season} Round ${add.round}`}</div>
-                            );
-                        })}
-                    </>
-                ))}
+            <div key={t.transaction_id} className={styles.singleton}>
+                {t.roster_ids.map((userId, idx) => {
+                    const user = findUser(userId);
+                    if (!user) {
+                        return <></>;
+                    }
+
+                    const adds = addsByUser.get(user);
+                    const drops = dropsByUser.get(user);
+
+                    return (
+                        <>
+                            <div
+                                className={
+                                    styles.teamLabel +
+                                    (idx > 0 ? ` ${styles.notFirst}` : '')
+                                }
+                                onClick={() => {
+                                    const teamId = allUsers.findIndex(
+                                        u => u.user_id === user.user_id
+                                    );
+                                    navigate(
+                                        `../team?${LEAGUE_ID}=${leagueId}&${TEAM_ID}=${teamId}`
+                                    );
+                                }}
+                            >
+                                {user.display_name}
+                            </div>
+                            {!!adds && (
+                                <>
+                                    {addedText}
+                                    {adds.map(addDropPiece)}
+                                </>
+                            )}
+                            {!!drops && (
+                                <>
+                                    {droppedText}
+                                    {drops.map(addDropPiece)}
+                                </>
+                            )}
+                        </>
+                    );
+                })}
             </div>
         );
     }
