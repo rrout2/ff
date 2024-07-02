0a286cc923885d01b9db50cd9516e50f0a07c02a
Rishav Rout <rout.rishav@gmail.com>
2024-07-02 14:18:07 -0700

    can calulate score and grade

--- src/components/Blueprint/BlueprintGenerator/modules/PositionalGrades/PositionalGrades.tsx
+++ src/components/Blueprint/BlueprintGenerator/modules/PositionalGrades/PositionalGrades.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import styles from './PositionalGrades.module.css';
-import {Roster, User} from '../../../../../sleeper-api/sleeper-api';
+import {Player, Roster, User} from '../../../../../sleeper-api/sleeper-api';
 import {usePlayerData, usePlayerValues} from '../../../../../hooks/hooks';
-import {sortBySearchRank} from '../../../../Player/Search/PlayerSearch';
+import {FANTASY_POSITIONS, SUPER_FLEX_SET} from '../../../../../consts/fantasy';
 
 interface PositionalGradesProps {
     roster?: Roster;
     specifiedUser?: User;
 }
+
+const THRESHOLDS = new Map<string, number>([
+    ['QB', 250],
+    ['TE', 55],
+    ['RB', 120],
+    ['WR', 250],
+]);
 
 export default function PositionalGrades({
     roster,
@@ -15,23 +22,50 @@ export default function PositionalGrades({
 }: PositionalGradesProps) {
     const playerData = usePlayerData();
     const {getPlayerValue} = usePlayerValues();
-    // roster => playerIds => players => filter by position =>
-    //     get name => playerValues => sum => do calculation
 
-    function something() {
-        if (!playerData) return <></>;
-        return roster?.players
+    function scoreByPosition(pos: string) {
+        if (!SUPER_FLEX_SET.has(pos)) {
+            throw new Error(`Unknown position '${pos}'`);
+        }
+        if (!playerData || !roster) return 0;
+
+        return roster.players
             .map(playerId => playerData[playerId])
-            .sort(sortBySearchRank)
-            .map(player => {
+            .filter(
+                player => !!player && player.fantasy_positions.includes(pos)
+            )
+            .reduce((acc: number, player: Player) => {
                 const fullName = `${player.first_name} ${player.last_name}`;
-                return (
-                    <div>
-                        {fullName}: {getPlayerValue(fullName)?.Value}
-                    </div>
-                );
-            });
+                const playerValue = getPlayerValue(fullName);
+                if (!playerValue) {
+                    throw new Error(
+                        `cannot find PlayerValue for player with name = '${fullName}'`
+                    );
+                }
+                return acc + +playerValue.Value;
+            }, 0);
     }
 
-    return <div className={styles.PositionalGrades}>{something()}</div>;
+    function gradeByPosition(pos: string, score: number) {
+        if (!SUPER_FLEX_SET.has(pos)) {
+            throw new Error(`Unknown position '${pos}'`);
+        }
+        return Math.round((10 * score) / THRESHOLDS.get(pos)!);
+    }
+
+    function sanityCheck() {
+        if (!playerData) return <></>;
+
+        return FANTASY_POSITIONS.map(position => {
+            const score = scoreByPosition(position);
+            const grade = gradeByPosition(position, score);
+            return (
+                <div>
+                    {position}: {score} - {grade}/10
+                </div>
+            );
+        });
+    }
+
+    return <div>{sanityCheck()}</div>;
 }
