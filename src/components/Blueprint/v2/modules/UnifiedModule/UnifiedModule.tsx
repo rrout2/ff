60ce689d1f940c86eae728384133fa46934f2d26
Rishav Rout <rout.rishav@gmail.com>
2024-08-23 15:45:32 -0700

    suggestedMovesGraphic class

--- src/components/Blueprint/v2/modules/UnifiedModule/UnifiedModule.tsx
+++ src/components/Blueprint/v2/modules/UnifiedModule/UnifiedModule.tsx
@@ -79,6 +79,7 @@ export default function UnifiedModule({
                     'rosterGraphic',
                     'settingsGraphic',
                     'cornerstonesGraphic',
+                    'suggestedMovesGraphic',
                 ]}
                 zipName={`${teamName}_unified.zip`}
             />
