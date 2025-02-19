7e57e60b91f8cbc0343f90a8a49f5ce492631d38
Rishav Rout <rout.rishav@gmail.com>
2025-02-19 13:32:18 -0800

    new script parses

--- scripts/infinite_bp/parse_customer_data.py
+++ scripts/infinite_bp/parse_customer_data.py
@@ -5,16 +5,16 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the JSON file name
-json_file_name = 'domain_customer_info_feb15.json'
+json_file_name = 'domain_customer_info_feb16.json'
 
 # Construct the full path to the JSON file
 json_file_path = os.path.join(script_dir, json_file_name)
 
-league_id_key = "What is the Sleeper League ID for this team? (the number in the URL, on MOBILE its in your league settings)"
-team_id_key = "What is your Sleeper TEAM ID? (follow the link to find your team ID, it should be a 1-12 (or however many teams are in your league) number"
-sub_team_id_key = "\r\n\r\nFIND YOUR TEAM ID HERE\r\nTEAM ID FINDER"
-email_key = "What is your email address? (We will send your Infinite Blueprint to this email each month)"
-verified_key = 'Column 1\r'
+league_id_key = "league_id"
+team_id_key = "team_id"
+# sub_team_id_key = "\r\n\r\nFIND YOUR TEAM ID HERE\r\nTEAM ID FINDER"
+email_key = "email\r"
+# verified_key = 'Column 1\r'
 # Open and read the JSON file
 try:
     with open(json_file_path, 'r') as file:
@@ -24,13 +24,15 @@ except FileNotFoundError:
 except json.JSONDecodeError as e:
     print(f"Error: Failed to decode JSON - {e}")
 
-verified_data = [item for item in data if item[verified_key] == 'Verified\r']
+# verified_data = [item for item in data if item[verified_key] == 'Verified\r']
+verified_data = data
 
 league_ids = [str(item[league_id_key]) for item in verified_data]
 print('League IDs:')
 print(','.join(league_ids))
 print('\nTeam IDs:')
-team_ids = [str(item[team_id_key][sub_team_id_key]) for item in verified_data]
+# team_ids = [str(item[team_id_key][sub_team_id_key]) for item in verified_data]
+team_ids = [str(item[team_id_key]) for item in verified_data]
 print(','.join(team_ids))
 print('\nEmails:')
 emails = [item[email_key].strip() for item in verified_data]
