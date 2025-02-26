37722dd89406311212451c7f2e38f7a12cb0fb32
Rishav Rout <rout.rishav@gmail.com>
2025-02-26 14:02:42 -0800

    print folder link

--- scripts/infinite_bp/monthly_image_sender.py
+++ scripts/infinite_bp/monthly_image_sender.py
@@ -188,6 +188,7 @@ def main():
         # Authenticate
         uploader.authenticate()
         folder_id = uploader.create_or_get_folder(args.folder_name)
+        print(f"Folder link: https://drive.google.com/drive/folders/{folder_id}")
         for i in range(len(sender.league_id_list)):
             if sender.league_id_list[i] == '' or sender.league_id_list[i] == None:
                 continue
