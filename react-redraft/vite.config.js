b1f3723f7dd392b7006ca1a9f96e10a802b4879f
cjtags151508 <battagliacj15@gmail.com>
2025-08-22 12:18:23 -0400

    Deploy react-redraft subfolder to GitHub Pages

--- react-redraft/vite.config.js
+++ react-redraft/vite.config.js
@@ -1,7 +1,10 @@
+// react-redraft/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Pages URL will be: https://cjtags151508.github.io/ff/react-redraft/
+  base: '/ff/react-redraft/',
+  build: { outDir: 'dist' } // default is fine; keep 'dist'
 })
